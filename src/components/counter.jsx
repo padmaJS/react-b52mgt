9bbc32d22d0f09be2fd7fa0b757397167c7bddcf
padmaJS <padshak230@gmail.com>
2021-04-20 12:39:50 +0545

    1 file modified, 1 added

---
+++ src/components/counter.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    counter: 0
+  };
+  updateCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  decreaseCounter = () => {
+    this.state.counter > 0
+      ? this.setState({ counter: this.state.counter - 1 })
+      : this.setState({ counter: 0 });
+  };
+  count() {
+    const { counter } = this.state;
+    return counter === 0 ? "Zero" : counter;
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+          You've pressed the button {this.count()} time
+          {this.state.counter > 1 && "s"}
+        </h1>
+        <button onClick={this.updateCounter}>Increase</button>
+        <button onClick={this.decreaseCounter}>Decrease</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+import Counter from './components/counter.jsx';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
