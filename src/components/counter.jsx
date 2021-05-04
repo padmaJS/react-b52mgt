0a88523ebf5c575fc8e07d66d15c3cfe60f84754
padmaJS <padshak230@gmail.com>
2021-05-04 21:05:52 +0545

    1 file modified

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,25 +2,32 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    timer: 0
+    seconds: 0,
+    minutes: 0
   };
   starter = () => {
-    this.timeStart = setInterval(() => this.setState({timer : this.state.timer + 1}),1000);
+    this.timeStart = setInterval(() => this.setState({seconds : this.state.seconds + 1}),1000);
   }
   stopper = () => {
     clearInterval(this.timeStart);
   }
   reset = () => {
     clearInterval(this.timeStart);
-    this.setState({timer : 0});   
+    this.setState({seconds : 0});   
+  }
+  showSeconds(){
+
+  }
+  showMinutes(){
+    
   }
   render() {
     return (
       <div>
-        <h1>{this.state.timer}</h1>
+        <h1>{this.showMinutes()}:{this.showSeconds()}</h1>
         <button onClick = {this.starter}>Start</button>
         <button onClick = {this.stopper}>Stop</button>
-        <button onClick = {this.reset} disabled = {this.state.timer === 0}>Reset</button>
+        <button onClick = {this.reset} disabled = {this.state.seconds === 0}>Reset</button>
       </div>
     );
   }
