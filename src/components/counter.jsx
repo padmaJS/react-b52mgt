e513051b0e038ec03e3082747b135afbdb606992
padmaJS <padshak230@gmail.com>
2021-05-06 19:02:48 +0545

    1 file modified

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,25 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
+    timerOff : false,
     seconds: 0,
     minutes: 0
   };
   starter = () => {
     this.timeStart = setInterval(() => this.setState({seconds : this.state.seconds + 1}),1000);
+    if(!this.state.timerOff){
+      this.setState({timerOff : true})
+    }
   }
   stopper = () => {
+    if(this.state.timerOff){
+      this.setState({timerOff : false})
+    }
     clearInterval(this.timeStart);
   }
   reset = () => {
     clearInterval(this.timeStart);
+    this.setState({timerOff : false})
     this.setState({seconds : 0});   
   }
   showSeconds(){
@@ -28,8 +36,8 @@ class Counter extends Component {
   }
   showMinutes(){
     if(this.state.minutes < 10){
-        return "0" + this.state.minutes;
-      }else{
+      return "0" + this.state.minutes;
+    }else{
       return this.state.minutes;
     }
   }
@@ -37,8 +45,8 @@ class Counter extends Component {
     return (
       <div>
         <h1>{this.showMinutes()}:{this.showSeconds()}</h1>
-        <button onClick = {this.starter}>Start</button>
-        <button onClick = {this.stopper}>Stop</button>
+        <button onClick = {this.starter} disabled={this.state.timerOff}>Start</button>
+        <button onClick = {this.stopper} disabled={!this.state.timerOff}>Stop</button>
         <button onClick = {this.reset} disabled = {this.state.seconds === 0}>Reset</button>
       </div>
     );
